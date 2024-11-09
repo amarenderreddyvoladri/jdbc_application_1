5cd46ea05f32c08f6bb7b84efd82822827c5fb7d
amarenderreddyvoladri <amarender.voladri@gmail.com>
2024-11-09 17:46:49 +0530

    first commit jdbc_application_1

---
+++ src/main/java/com/edubridge/jdbc_application_1/App.java
@@ -0,0 +1,13 @@
+package com.edubridge.jdbc_application_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
